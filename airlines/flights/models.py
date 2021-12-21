cf963efb64cdcb9a342040655ace3eae8985e23e
Glakuta <92273750+Glakuta@users.noreply.github.com>
2021-12-21 21:30:08 +0100

    Revert "Add airports, modify Flight"
    This reverts commit d81173501dd55dd4a982b7148a26dd4dfd2482ca.

--- airlines/flights/models.py
+++ airlines/flights/models.py
@@ -1,20 +1,9 @@
 from django.db import models
-from django.db.models.deletion import CASCADE
 from django.db.models.fields import CharField
-from django.db.models.query_utils import check_rel_lookup_compatibility
 
 # Create your models here.
 
-class Airport(models.Model):
-    code = models.CharField (max_length=3)
-    city = models.CharField (max_length=64)
-    def __str__(self):
-        return f"{self.city} ({self.code})"
-
 class Flight(models.Model):
-    origin = models.ForeignKey (Airport, on_delete= CASCADE, related_name="departures")
-    destination = models.ForeignKey (Airport, on_delete= CASCADE, related_name="arrivals")
+    origin = CharField (max_length=64)
+    destination = CharField (max_length=64)
     duration =models.IntegerField()
-    
-    def __str__(self):
-      return f"{self.id}: {self.origin} to {self.destination}"
